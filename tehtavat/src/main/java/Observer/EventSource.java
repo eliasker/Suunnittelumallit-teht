d2701d32758767a6444651245228939eb4132b00
eliasker <eliasker@gmail.com>
2020-03-29 19:07:33 +0300

    4) Thred sleeps less after 10 seconds in timer has passed

--- tehtavat/src/main/java/Observer/EventSource.java
+++ tehtavat/src/main/java/Observer/EventSource.java
@@ -2,18 +2,37 @@ package Observer;
 
 public class EventSource extends Thread {
   private ClockTimer timer;
+  private int DELAY;
+  private boolean changedDelay;
 
   public EventSource(ClockTimer timer) {
     this.timer = timer;
+    DELAY = 1000;
+    changedDelay = false;
   }
 
   @Override
   public void run() {
-    while (true) {
+    while (!changedDelay) {
       try {
-        sleep(1000);
+        sleep(DELAY);
         timer.tick();
-        if (timer.getHours() > 2)
+        if (timer.getSeconds() > 10 && changedDelay == false) {
+          System.out.println("Ajan rakenne on muuttunut.");
+          sleep(DELAY);
+          changedDelay = true;
+          DELAY = 10;
+          break;
+        }
+      } catch (Exception e) {
+        System.out.println(e);
+      }
+    }
+    while (changedDelay) {
+      try {
+        sleep(DELAY);
+        timer.tick();
+        if (timer.getHours() >= 2)
           break;
       } catch (Exception e) {
         System.out.println(e);
